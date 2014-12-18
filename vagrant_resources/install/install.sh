5fcbbea7db7b86c38f2705521e59246d0a7efa7e
rajeshvaya <vaya.rajesh@gmail.com>
2014-12-18 09:13:27 +0400

    Get ready with the initial setup

---
+++ vagrant_resources/install/install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+echo "==========================================="
+echo "Provisioning virtual machine..."
+echo "==========================================="
+
+# Install essential packages from Apt
+echo "Updating apt-get..."
+apt-get update -y
+
+# python-setuptools
+echo "=============================="
+echo "Installing python-setuptools"
+echo "=============================="
+apt-get install python-setuptools -y
+
+#apache
+if ! command -v apache2; then
+    echo "=============================="
+    echo "Installing Apache2"
+    echo "=============================="
+    sudo apt-get install apache2 -y
+fi
+
+#git
+if ! command -v git; then
+    echo "=============================="
+    echo "Installing Git"
+    echo "=============================="
+    sudo apt-get install git -y
+fi
+
+#sqlite3
+if ! command -v sqlite3; then
+    echo "=============================="
+    echo "Installing sqlite3"
+    echo "=============================="
+    sudo apt-get install sqlite3 -y
+fi
+
+# pip
+if ! command -v pip; then
+    echo "=============================="
+    echo "Installing pip..."
+    echo "=============================="
+    easy_install -U pip
+fi
+
+#virtualenv
+if [[ ! -f /usr/local/bin/virtualenv ]]; then
+    echo "==========================================="
+    echo "Installing virtualenv virtualwrapper..."
+    echo "==========================================="
+    pip install virtualenv virtualenvwrapper stevedore virtualenv-clone
+fi
+
+
+# install image manipulation libraries 
+
+apt-get install libjpeg-dev -y
+apt-get install zlib1g-dev -y
+apt-get install libpng12-dev -y
